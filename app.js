3ec174bf2b4c02ac08a29a819b0be482a87d8776
Lvcios <lfloresg0801@gmail.com>
2018-06-13 14:16:55 -0500

    resultado

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ function read(a) {
         if (a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
             html += "<a target='_blank' href='" + a + "'>" + a + "</a><br>";
         html += "<b>" + htmlEntities(a) + "</b><br><br>";
-        document.getElementById("div-result").innerHTML = html;
+        document.getElementById("div-result").innerHTML = "Resultado: ";
+        document.getElementById("div-result").innerHTML += html;
     }
     catch (error) {
         document.getElementById("div-errors").innerHTML = error
