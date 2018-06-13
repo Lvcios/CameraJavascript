8e3d313e5bda7b7473f6b29adaa4bfdd074f3c54
Lvcios <lfloresg0801@gmail.com>
2018-06-13 16:22:44 -0500

    url

--- app.js
+++ app.js
@@ -9,19 +9,10 @@ var constraints = {
 }
 
 
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-
-
 function read(a) {
     try{
-        var html = "<br>";
-        if (a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
-            html += "<a target='_blank' href='" + a + "'>" + a + "</a><br>";
-        html += "<b>" + htmlEntities(a) + "</b><br><br>";
         document.getElementById("div-result").innerHTML = "Resultado: ";
-        document.getElementById("div-result").innerHTML += html;
+        document.getElementById("div-result").innerHTML += a;
     }
     catch (error) {
         document.getElementById("div-errors").innerHTML = error
@@ -36,6 +27,8 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
          urlMedia = window.URL.createObjectURL(stream)
          video.src = urlMedia;
          video.play();
+         qrcode.decode(urlMedia)
+         qrcode.callback = read;
      })
      .catch(function(error){
          document.getElementById("div-errors").innerHTML = ''
