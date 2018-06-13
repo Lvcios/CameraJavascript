5c63a8e8c7d3beeb22adae02b46c4344d744dc24
Lvcios <lfloresg0801@gmail.com>
2018-06-13 13:56:38 -0500

    add qr libraries

--- app.js
+++ app.js
@@ -8,16 +8,30 @@ var constraints = {
     }
 }
 
+function read(a) {
+    var html = "<br>";
+    if (a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
+        html += "<a target='_blank' href='" + a + "'>" + a + "</a><br>";
+    html += "<b>" + htmlEntities(a) + "</b><br><br>";
+    document.getElementById("div-result").innerHTML = html;
+}
+
 var video = document.getElementById('video');
 
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
      navigator.mediaDevices.getUserMedia(constraints)
-     .then(function(stream) {
-         video.src = window.URL.createObjectURL(stream);
+     .then(function (stream) {
+         var url = window.URL.createObjectURL(stream)
+         video.src = url;
          video.play();
+         qrcode.decode(url)
+         qrcode.callback = read;
      })
      .catch(function(error){
          document.getElementById("div-errors").innerHTML = ''
          document.getElementById("div-errors").innerHTML = error
      })
 }
+
+
+
