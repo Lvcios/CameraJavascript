21ad070e11977b662f7335d684609eaeb5bbf4f5
Lvcios <lfloresg0801@gmail.com>
2018-06-13 16:14:39 -0500

    funciones

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ var constraints = {
     }
 }
 
+var video = document.getElementById('video');
+
+function initCanvas(w, h) {
+    gCanvas = document.getElementById("qr-canvas");
+    gCanvas.style.width = w + "px";
+    gCanvas.style.height = h + "px";
+    gCanvas.width = w;
+    gCanvas.height = h;
+    gCtx = gCanvas.getContext("2d");
+    gCtx.clearRect(0, 0, w, h);
+}
+
 
 function htmlEntities(str) {
     return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
@@ -30,22 +42,63 @@ function read(a) {
     }
 }
 
-var video = document.getElementById('video');
+function captureToCanvas() {
+    if (stype != 1)
+        return;
+    if (gUM) {
+        try {
+            gCtx.drawImage(v, 0, 0);
+            try {
+                qrcode.decode();
+            }
+            catch (e) {
+                console.log(e);
+                setTimeout(captureToCanvas, 500);
+            };
+        }
+        catch (e) {
+            console.log(e);
+            setTimeout(captureToCanvas, 500);
+        };
+    }
+}
 
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-     navigator.mediaDevices.getUserMedia(constraints)
-     .then(function (stream) {
-         video.src = window.URL.createObjectURL(stream);
-         video.play();
-         qrcode.decode(window.URL.createObjectURL(stream))
-         
-     })
-     .catch(function(error){
-         document.getElementById("div-errors").innerHTML = ''
-         document.getElementById("div-errors").innerHTML = error
-     })
+function setwebcam() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+            qrcode.decode(window.URL.createObjectURL(stream))
 
-     setTimeout(function () {
-         qrcode.callback = read;
-     }, 1000)
+        })
+        .catch(function (error) {
+            document.getElementById("div-errors").innerHTML = ''
+            document.getElementById("div-errors").innerHTML = error
+        })
+
+        setTimeout(captureToCanvas, 500);
+    }
+    else {
+        console.log("no navigator.mediaDevices.enumerateDevices");
+        setwebcam2(options);
+    }
+
+}
+
+
+
+
+function load() {
+    if (isCanvasSupported() && window.File && window.FileReader) {
+        initCanvas(800, 600);
+        qrcode.callback = read;
+        setwebcam();
+    }
+    else {
+        document.getElementById("mainbody").style.display = "inline";
+        document.getElementById("mainbody").innerHTML = '<p id="mp1">QR code scanner for HTML5 capable browsers</p><br>' +
+        '<br><p id="mp2">sorry your browser is not supported</p><br><br>' +
+        '<p id="mp1">try <a href="http://www.mozilla.com/firefox"><img src="firefox.png"/></a> or <a href="http://chrome.google.com"><img src="chrome_logo.gif"/></a> or <a href="http://www.opera.com"><img src="Opera-logo.png"/></a></p>';
+    }
 }
