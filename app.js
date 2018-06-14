fbffed1330a62388b3b1fc8962edc37fce77daa5
Lvcios <lfloresg0801@gmail.com>
2018-06-14 14:26:01 -0500

    img div dinámico

--- app.js
+++ app.js
@@ -9,16 +9,22 @@ var constraints = {
 }
 const codeReader = new ZXing.BrowserQRCodeReader()
 var video = document.getElementById('video');
-var img = document.getElementById('img');
+var canvas = document.createElement('canvas');
 var errorDiv = document.getElementById('div-errors');
 var urlMedia
 
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
      navigator.mediaDevices.getUserMedia(constraints)
      .then(function (stream) {
          urlMedia = window.URL.createObjectURL(stream)
          video.src = urlMedia;
          video.play();
+         canvas.width = video.width;
+         canvas.height = video.height;
+         var ctx = canvas.getContext('2d');
+         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+         var dataURI = canvas.toDataURL('image/jpeg');
+         document.getElementById("img-div").innerHTML = '<img id="img" width="640" height="480" src="' + dataURI + '"/>'
      })
      .catch(function(error){
          errorDiv.innerHTML = error
@@ -27,26 +33,36 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
 document.getElementById("btn-decode").addEventListener("click", function () {
     try {
-        var canvas = document.createElement('canvas');
-        canvas.width = video.width;
-        canvas.height = video.height;
-        var ctx = canvas.getContext('2d');
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-        var dataURI = canvas.toDataURL('image/jpeg');
+        //var canvas = document.createElement('canvas');
+        //canvas.width = video.width;
+        //canvas.height = video.height;
+        //var ctx = canvas.getContext('2d');
+        //ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        //var dataURI = canvas.toDataURL('image/jpeg');
         
-        img.width = video.width;
-        img.height = video.height;
-        img.src = dataURI
-        codeReader.decodeFromImage(img).then((result) => {
+        //img.width = video.width;
+        //img.height = video.height;
+
+        var dataURI = canvas.toDataURL('image/jpeg');
+        document.getElementById("img-div").innerHTML = '<img id="img" width="640" height="480" src="' + data
+
+        //$("img").attr("src", "qrcode.png")
+        codeReader.decodeFromImage("img").then((result) => {
             console.log(result)
             console.log(JSON.stringify(result))
             errorDiv.innerHTML = JSON.stringify(result)
-            img.src = 'qrcode.png'
+
         }).catch((error) => {
             errorDiv.innerHTML = JSON.stringify(error)
         })
+
     }
     catch (error) {
         errorDiv.innerHTML = JSON.stringify(error)
     }
 });
+
+
+//setInterval(function(){
+//    img.src = 'qrcode.png?date=' + new Date().getTime();    
+//},500)
