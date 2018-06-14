2b495c1d97dcf0e30eea2cf3942804d62e416ac0
Lvcios <lfloresg0801@gmail.com>
2018-06-14 12:34:22 -0500

    captura interna

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var constraints = {
 }
 const codeReader = new ZXing.BrowserQRCodeReader()
 var video = document.getElementById('video');
-var img = document.getElementById('img')
 var errorDiv = document.getElementById('div-errors');
 var urlMedia
 
@@ -28,14 +27,15 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 document.getElementById("btn-decode").addEventListener("click", function () {
     try {
         var canvas = document.createElement('canvas');
-        canvas.width = 640;
-        canvas.height = 480;
+        canvas.width = video.width;
+        canvas.height = video.height;
         var ctx = canvas.getContext('2d');
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
         var dataURI = canvas.toDataURL('image/jpeg');
-
+        var img = document.createElement('img');
+        img.width = video.width;
+        img.height = video.height;
         img.src = dataURI
-
         codeReader.decodeFromImage(img).then((result) => {
             errorDiv.innerHTML = result.text
         }).catch((err) => {
